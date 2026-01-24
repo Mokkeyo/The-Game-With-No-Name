1bebff47c85d6f6684f5e797a6548573f77d0682
Mokkeyo <umutcanbektas1405@gmail.com>
2026-01-24 16:12:27 +0100

    ground_state refactor

--- The-Game-With-No-Name/ground_state.gd
+++ The-Game-With-No-Name/ground_state.gd
@@ -1,17 +1,26 @@
 extends PlayerState
 
+var dir: float
+
 func enter() -> void:
 	player.can_doublejump = true
 	player.play_animation(player.player_strings["idle"])
 
 func handle_input() -> void:
+	dir = int(Input.is_action_pressed(player.inputs["left"])) \
+		- int(Input.is_action_pressed(player.inputs["right"]))
+	
 	if Input.is_action_just_pressed(player.inputs["jump"]):
-		player.change_state("jump")
+		player.jump(player.JUMP_POWER)
+		player.change_state("air")
+	
+	if Input.is_action_just_pressed(player.inputs["attack"]):
+		player.sword.attack()
+
+	if Input.is_action_just_pressed(player.inputs["wand"]):
+		player.cast_spell()
 
 func physics_update(_delta: float) -> void:
-	var dir: int = int(Input.is_action_pressed(player.inputs["left"])) \
-				- int(Input.is_action_pressed(player.inputs["right"]))
-	
 	if dir == 0:
 		player.velocity.x = lerp(player.velocity.x, 0.0, 0.4)
 		player.play_animation(player.player_strings["idle"])
